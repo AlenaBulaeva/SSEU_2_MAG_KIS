2dae4c942b02dbbd98737b7ebcf4b2d55c7ed3b1
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-10-13 20:23:17 +0400

    Итоговое сохранение готовой программы

--- 123/ReadAndSum.cs
+++ 123/ReadAndSum.cs
@@ -7,7 +7,18 @@ namespace ReadAndSum
     class Program
     {
         static void Main(string[] args)
-        {
+        {//объявляем переменные 
+            int a; 
+            int b;
+            //обеспечиваем ввод переменных с консоли
+            int.TryParse(Console.ReadLine(), out a);
+            int.TryParse(Console.ReadLine(), out b);
+            //определяем функцию сложения
+            int sum = a + b;
+            //выводим на экран значение а, b и их сумму
+            Console.WriteLine(a + "+" + b + "=" + sum);
+            //чтобы система не вылетала
+            Console.ReadKey();
         }
     }
 }
