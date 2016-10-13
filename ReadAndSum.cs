9eb18a76ffc25747ec2ef953d48a062f91fea52f
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-10-13 20:26:50 +0400

    Сделала коммит, чтобы удаленная папка пропала

---
+++ ReadAndSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//объявляем переменные 
+            int a; 
+            int b;
+            //обеспечиваем ввод переменных с консоли
+            int.TryParse(Console.ReadLine(), out a);
+            int.TryParse(Console.ReadLine(), out b);
+            //определяем функцию сложения
+            int sum = a + b;
+            //выводим на экран значение а, b и их сумму
+            Console.WriteLine(a + "+" + b + "=" + sum);
+            //чтобы система не вылетала
+            Console.ReadKey();
+        }
+    }
+}
