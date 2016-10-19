7c12af320219f3a93e55d3ab39df32215715ea87
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-10-19 23:29:36 +0300

    Задание 1. Программа перевода градусов Цельсия в градусы Фаренгейта

---
+++ CalsAndFar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CelsAndFar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //объявляем переменные 
+            double c; 
+            //обеспечиваем ввод переменных с консоли
+            Console.Write("Введите температуру в градусах Цельсия: ");
+            double.TryParse(Console.ReadLine(), out c);
+            //описываем функцию перевода градусов Цельсия в градусы Фаренгейта
+            double f = c * 9 / 5 + 32;
+            //выводим ответ на экран
+            Console.WriteLine("Температура по Фаренгейту равна: {0}", f);
+            //не позволяем программе вылетать сразу после вывода ответа
+            Console.ReadKey();
+        }
+    }
+}
