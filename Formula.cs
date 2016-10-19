84f9c9cd10191387d2172b703e4ccb4c7efa0e7a
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-10-19 23:44:04 +0300

    Задание 2. Написать программу, которая будет вычислять значение функции f(x) по введенному значению x

---
+++ Formula.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //объявляем переменные 
+            int a=19; 
+            int b=5;
+			int c=1993;
+			double x;
+            //обеспечиваем ввод переменных с консоли
+			Console.Write("Введите значение X: ");
+            double.TryParse(Console.ReadLine(), out x);
+            //определяем функцию
+            double fun = -(x/a*Math.Sqrt(Math.Pow(x, 2)+b*x))+c;
+            //выводим на экран значение функции при заданных значениях
+            Console.WriteLine("F(x)" + "=" + fun);
+            //чтобы система не вылетала
+            Console.ReadKey();
+        }
+    }
+}
