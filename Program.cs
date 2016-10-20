f7c415203cb7b123e48b27f39b9befd61a13afbc
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-10-20 19:51:49 +0400

    Л2.П2. Проверка введенных значений

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод исходных значений
+            int A, B;
+            if (int.TryParse(Console.ReadLine(), out A))
+            {
+                if (int.TryParse(Console.ReadLine(), out B))
+                {
+                    //максимальное значение
+                    int Max;
+                    //если больше А
+                    if (A >= B)
+                    {
+                        Max = A;
+                    }
+                    //иначе - больше В
+                    else
+                    {
+                        Max = B;
+                    };
+                    //вывод результата
+                    Console.WriteLine("Максимальное = " + Max);
+                }
+                else 
+                {
+                    Console.WriteLine("Неверное значение! Нужно ввести целое число.");
+                };
+            }
+            else
+            {
+                Console.WriteLine("Неверное значение! Нужно ввести целое число.");
+            };
+            //завершение программы
+            Console.ReadKey();
+            }
+    }
+}
+
