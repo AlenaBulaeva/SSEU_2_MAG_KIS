226ed97c945dfd6b6ad1825aa7d0d010b89c3c36
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-10-27 20:00:14 +0400

    Л2.П2 Максимальное из трех чисел через if

---
+++ Max3.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод исходных значений
+            int A, B, C;
+            if (int.TryParse(Console.ReadLine(), out A) && int.TryParse(Console.ReadLine(), out B) && int.TryParse(Console.ReadLine(), out C))
+            {
+                //максимальное значение
+                int Max;
+                //если А >= B
+                if (A >= B)
+                {
+                    //и если А >= С
+                    if (A >= C)
+                    {
+                        //то максимальное - А
+                        Max = A;
+                    }
+                    //иначе - C > A
+                    else
+                    {
+                        //максимальное - С
+                        Max = C;
+                    }
+                }
+                //иначе - больше В
+                else
+                {
+                    // и если B >= C
+                    if (B >= C)
+                    {
+                        //то максимальное В
+                        Max = B;
+                    }
+                    // иначе - С > В
+                    else
+                    {
+                        // максимальное - С
+                        Max = C;
+                    }
+                };
+                //вывод результата
+                Console.WriteLine("Максимальное = " + Max);
+            }
+            else
+            {
+                Console.WriteLine("Неверное значение! Нужно ввести целое число.");
+            };
+            //завершение программы
+            Console.ReadKey();
+        }
+    }
+}
