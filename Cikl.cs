c4118bf3635012057f3608298c00c6d5b36c1952
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-11-09 21:30:12 +0300

    Лекция 3. Пример 1. Вывод чисел

---
+++ Cikl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cikl
+{
+    class Cikl
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++) //пока i меньше 10
+            {
+                Console.WriteLine(i); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
