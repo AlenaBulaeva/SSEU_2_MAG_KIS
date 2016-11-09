c9de6635700951bad8bdada30b09623dfb4fb213
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-11-09 20:37:45 +0300

    Задание 5. Конвертер разных валют

--- Zadanie5.cs
+++ Zadanie5.cs
@@ -10,25 +10,48 @@ namespace Zadanie5
         static void Main(string[] args)
         {
             int m;
+            int n;
             double x;
             double result = 0;
-            Console.WriteLine("Введите сумму в рублях:");
-            if (double.TryParse(Console.ReadLine(), out x))
+            double rub;
+            Console.WriteLine("Введите номер валюты, которую необходимо перевести:");
+            Console.WriteLine("1 - доллар");
+            Console.WriteLine("2 - евро");
+            Console.WriteLine("3 - фунты");
+            Console.WriteLine("4 - иены");
+            if (int.TryParse(Console.ReadLine(), out m))
             {
+                Console.WriteLine("Введите число:");
+                if (double.TryParse(Console.ReadLine(), out x))
+                {
+                    switch (m)
+                    {
+                        case 1: result = x / 63.74; break;
+                        case 2: result = x / 70.45; break;
+                        case 3: result = x / 79.32; break;
+                        case 4: result = x / 1.62; break;
+                        default: Console.WriteLine("Ошибка! Валюта с данным номером не найдена"); break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Неверное значение! Введите число.");
+                };
+                rub = result;
                 Console.WriteLine("Выберете в какую валюту переводить:");
                 Console.WriteLine("1 - доллар");
                 Console.WriteLine("2 - евро");
                 Console.WriteLine("3 - фунты");
                 Console.WriteLine("4 - иены");
-                if (int.TryParse(Console.ReadLine(), out m))
+                if (int.TryParse(Console.ReadLine(), out n))
                 {
-                    switch (m)
+                    switch (n)
                     {
-                        case 1: result = x * 63.74; break;
-                        case 2: result = x * 70.45; break;
-                        case 3: result = x * 79.32; break;
-                        case 4: result = x * 1.62; break;
-                        default: Console.WriteLine("Введите число"); break;
+                        case 1: result = rub * 63.74; break;
+                        case 2: result = rub * 70.45; break;
+                        case 3: result = rub * 79.32; break;
+                        case 4: result = rub * 1.62; break;
+                        default: Console.WriteLine("Ошибка! Валюта с данным номером не найдена"); break;
                     }
                 }
                 else
