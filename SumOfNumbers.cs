8449a7e80d3a7153f784815f4b0ed8408f2a66ab
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-11-09 21:58:24 +0300

    Л3.П1 Суммирование чисел в цикле for

---
+++ SumOfNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SumOfNumbers
+{
+    class SumOfNumbers
+    {
+        static void Main(string[] args)
+        {
+            int N; //количество чисел
+            int x; //одно число
+            Console.WriteLine("Введите колличество чисел для суммирования:");
+            if (int.TryParse(Console.ReadLine(), out N))
+            {
+                int Sum = 0; //сумма чисел
+                //N раз
+                for (int i = 0; i < N; i++)
+                {
+                    //если введено корректное число
+                    Console.WriteLine("Введите число:");
+                    if (int.TryParse(Console.ReadLine(), out x))
+                    {
+                        Sum += x; //добавить к сумме
+                        /* DEBUG INFO
+                        Console.WriteLine("{" + Sum + "}");
+                        //*/
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверное значение! Введите целое значение!");
+                        i--;
+                    };
+                };
+                //вывод результата
+                Console.WriteLine("Сумма: " + Sum);
+            };
+            //завершение рограммы
+            Console.ReadKey();
+        }
+    }
+}
