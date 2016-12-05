06c121091500735b08ac0b3fd13f5b844879c30b
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-12-05 20:57:23 +0300

    Л3.З2.Подсчет суммы и количества цифр числа

---
+++ Lek3Zad2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lek3Zad2
+{
+    class Lek3Zad2
+    {
+        static void Main(string[] args)
+        {
+            int col = 0;
+            int sum = 0;
+            int j;
+            Console.Write("Введите число: ");
+            if (int.TryParse(Console.ReadLine(), out j))
+            {
+                do
+                {
+                    sum += j % 10;
+                    col++;
+                    j = j / 10;
+                }
+                while (j / 10 != 0);
+                sum += j;
+                col++;
+            }
+            else
+            {
+                Console.WriteLine("Введите целое число!");
+            };
+            Console.WriteLine("Cумма: " + sum);
+            Console.WriteLine("Количество: " + col);
+            Console.ReadKey();
+        }
+    }
+}
