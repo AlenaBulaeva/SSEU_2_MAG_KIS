b73c5252dde0d3aa654bf318ed404a76f46cd7e2
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-12-08 19:55:18 +0400

    Л4.П1 Фамилия, имя, отчество, если нет отчества

--- FIO.cs
+++ FIO.cs
@@ -14,24 +14,44 @@ namespace FIO
             string otchestvo = Console.ReadLine();
             Console.Write("Введите фамилию: ");
             string familiya = Console.ReadLine();
+            string otchestvoSokr;
             //И. О. Фамилия
-            Console.WriteLine(imya[0] + "." + otchestvo[0] + "." + familiya);
-            Console.WriteLine(imya[0].ToString().ToUpper() + "." //И.
+            if (!string.IsNullOrEmpty(otchestvo)) //если ввели отчество
+            {
+                otchestvoSokr = otchestvo[0].ToString().ToUpper() + ". "; //то делаем сокращение
+                Console.WriteLine(imya[0].ToString().ToUpper() + "." //И.
                               + otchestvo[0].ToString().ToUpper() + "." //O.
                               + familiya[0].ToString().ToUpper() //Ф
                               + familiya.Substring(1).ToLower()); //амилия
-            Console.WriteLine(familiya[0].ToString().ToUpper() //Ф
+                Console.WriteLine(familiya[0].ToString().ToUpper() //Ф
                               + familiya.Substring(1).ToLower() + "." //амилия
                               + imya[0].ToString().ToUpper() + "." //И.
                               + otchestvo[0].ToString().ToUpper());  //O.
-            Console.WriteLine(imya[0].ToString().ToUpper()  //И
-                              + imya.Substring(1).ToLower() + "." //мя
-                              + otchestvo[0].ToString().ToUpper() //O
-                              + otchestvo.Substring(1).ToLower() + "." //тчество
-                              + familiya.ToUpper()); //ФАМИЛИЯ
-            Console.WriteLine(familiya.Substring(0, 3) + "_"  //фам_
+                Console.WriteLine(imya[0].ToString().ToUpper()  //И
+                             + imya.Substring(1).ToLower() + "." //мя
+                             + otchestvo[0].ToString().ToUpper() //O
+                             + otchestvo.Substring(1).ToLower() + "." //тчество
+                             + familiya.ToUpper()); //ФАМИЛИЯ
+                Console.WriteLine(familiya.Substring(0, 3) + "_"  //фам_
                               + imya.Substring(0, 1) + "_" //и_
                               + otchestvo.Substring(0, 1).ToLower()); //о
+            }
+            else //иначе - отчество не ввели
+            {
+                otchestvoSokr = ""; // его сокращение - пустая строка
+                //выводим без отчества
+                Console.WriteLine(imya[0].ToString().ToUpper() + "." //И.
+                                  + familiya[0].ToString().ToUpper() //Ф
+                                  + familiya.Substring(1).ToLower()); //амилия
+                Console.WriteLine(familiya[0].ToString().ToUpper() //Ф
+                              + familiya.Substring(1).ToLower() + "." //амилия
+                              + imya[0].ToString().ToUpper()); //И.
+                Console.WriteLine(imya[0].ToString().ToUpper()  //И
+                             + imya.Substring(1).ToLower() + "." //мя
+                             + familiya.ToUpper()); //ФАМИЛИЯ
+                Console.WriteLine(familiya.Substring(0, 3) + "_"  //фам_
+                              + imya.Substring(0, 1)); //и_
+            };
             Console.ReadKey();
         }
     }
