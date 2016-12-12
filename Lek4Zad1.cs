0c8bebab53e3edc59041911449bd53cf43236443
AlenaBulaeva <Alenka-A_63@mail.ru>
2016-12-12 23:52:57 +0300

    Лекция4.Задание1.Перевод в кодировку Unicode

---
+++ Lek4Zad1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lek4Zad1
+{
+    class Lek4Zad1
+    {
+        static void Main(string[] args)
+        {
+            char a;
+            Console.WriteLine("Нажмите любую клавишу");
+            char.TryParse(Console.ReadLine(), out a);
+            int b;
+            b = Convert.ToInt32(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+
+        }
+    }
+}
